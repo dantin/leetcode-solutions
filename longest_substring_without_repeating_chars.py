6b4cc06c7f2cbe8e195c9f6d47883a3a4c39072e
david <chengjie.ding@gmail.com>
2015-10-25 23:14:31 +0800

    solution 3

---
+++ longest_substring_without_repeating_chars.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Given a string, find the length of the longest substring without repeating characters. For example, the longest
+substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring
+is "b", with the length of 1.
+"""
+
+
+class Solution(object):
+    @classmethod
+    def length_of_longest_substring(cls, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        slow = 0
+        max_len = 0
+        hash_table = [-1] * 256
+        for fast, c in enumerate(s):
+            if hash_table[ord(c)] != -1:
+                while slow <= hash_table[ord(c)]:
+                    hash_table[ord(s[slow])] = -1
+                    slow += 1
+            if fast - slow + 1 > max_len:
+                max_len = fast - slow + 1
+            hash_table[ord(c)] = fast
+        return max_len
+
+
+if __name__ == '__main__':
+    input_str = 'abcabcbb'
+    length = Solution.length_of_longest_substring(input_str)
+    print(length)
