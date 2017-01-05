4873c12a15e64fa74e10667fb17bdb142180e7e6
david.ding <chengjie.ding@gmail.com>
2017-01-05 13:50:29 +0800

    python solution of hamming distance

---
+++ python/hamming_distance.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        d = 0
+        xor = x ^ y
+        while xor != 0:
+            d += xor & 1;
+            xor /= 2;
+
+        return d
+
+
+if __name__ == '__main__':
+    x, y = 1, 4
+    print "x = %d, y = %d" % (x, y)
+
+    solution = Solution()
+    print solution.hammingDistance(x, y)
