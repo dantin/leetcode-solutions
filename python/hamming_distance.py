8dd58cd9b6ff3519b9e50efb344d03aa00a1b6de
david.ding <chengjie.ding@gmail.com>
2017-01-05 13:57:55 +0800

    add new python solution of hamming distance

--- python/hamming_distance.py
+++ python/hamming_distance.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
 class Solution(object):
+
     def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x^y).count('1')
+
+    def hammingDistance1(self, x, y):
         """
         :type x: int
         :type y: int
