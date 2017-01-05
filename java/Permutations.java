e5c337026bce06d7b911f5aa85dbdf788a33fd3e
david.ding <chengjie.ding@gmail.com>
2017-01-05 14:54:58 +0800

    solution of permutations

--- java/Permutations.java
+++ java/Permutations.java
@@ -1,18 +1,39 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class Permutations {
 
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        List<List<Integer>> ans = new LinkedList<>();
+        permute(nums, 0, ans);
+        return ans;
+    }
+
+    private void permute(int[] nums, int n, List<List<Integer>> ans) {
+        if (n == nums.length) {
+            ans.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+        }
+        for (int i = n; i < nums.length; i++) {
+            swap(nums, i, n);
+            permute(nums, n + 1, ans);
+            swap(nums, i, n);
+        }
+    }
+
+    private void swap(int[] nums, int to, int from) {
+        int tmp = nums[from];
+        nums[from] = nums[to];
+        nums[to] = tmp;
     }
 
     public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3};
-        System.out.println(Arrays.toString(nums));
+        System.out.println("Input: " + Arrays.toString(nums));
 
         Permutations solution = new Permutations();
-        for(List<Integer> perm : solution.permute(nums)) {
+        for (List<Integer> perm : solution.permute(nums)) {
             System.out.println(perm);
         }
     }
