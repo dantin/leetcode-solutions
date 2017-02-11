f890da01a51ff5128d16e69647dfd44bfcf777f4
david.ding <chengjie.ding@gmail.com>
2017-02-11 15:07:19 +0800

    refactoring code

--- java/RansomNote.java
+++ java/RansomNote.java
@@ -5,23 +5,18 @@ import java.util.stream.Collectors;
 public class RansomNote {
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Long> magazineFrequency = magazine.chars().mapToObj(i -> (char)i).collect(
-            Collectors.groupingBy(c -> c, Collectors.counting()));
-        Map<Character, Long> ransomFrequency = ransomNote.chars().mapToObj(i -> (char)i).collect(
-            Collectors.groupingBy(c -> c, Collectors.counting()));
-        for(Map.Entry<Character, Long> entry : ransomFrequency.entrySet()) {
-            Character key = entry.getKey();
-            Long frequency = entry.getValue();
-
-            if(!magazineFrequency.containsKey(key) || magazineFrequency.get(key) < frequency) {
+        int[] cache = new int[255];
+        for (char c : magazine.toCharArray()) {
+            cache[c]++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            if (cache[c] > 0) {
+                cache[c]--;
+            } else {
                 return false;
             }
         }
         return true;
-        
-        // frequentChars.forEach((k, v) -> System.out.println(k + ":" + v));
-
-        // return true;
     }
 
     public static void main(String[] args) {
