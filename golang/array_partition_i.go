3c6673aa168b141db01fd93b48dee29771545e7c
david.ding <chengjie.ding@gmail.com>
2017-05-23 15:47:30 +0800

    array partition i in go

---
+++ golang/array_partition_i.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		if i%2 == 0 {
+			sum += nums[i]
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	nums := []int{1, 4, 3, 2}
+	fmt.Println(arrayPairSum(nums))
+}
