d43e4715ffa54d9844bbf19174e4b4a8cf7e0c5d
david.ding <chengjie.ding@gmail.com>
2017-06-01 18:54:57 +0800

    refactoring code

---
+++ golang/reverse_words_in_a_string_iii.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func reverseWords(s string) string {
+	buf := []rune(s)
+
+	for i := 0; i < len(buf); i++ {
+		if buf[i] != ' ' {
+			j := i
+			for j < len(buf) && buf[j] != ' ' {
+				j++
+			}
+			reverse(buf, i, j-1)
+			i = j
+		}
+	}
+
+	return string(buf)
+}
+
+func reverse(s []rune, i int, j int) {
+	for ; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	input := "Let's take LeetCode contest"
+	fmt.Println(input)
+	fmt.Println(reverseWords(input))
+}
