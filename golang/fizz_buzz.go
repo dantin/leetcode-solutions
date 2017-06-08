c143d283b17e44814264324090d62ffb671f5f81
david.ding <chengjie.ding@gmail.com>
2017-06-08 13:24:10 +0800

    solution of fizz buzz

---
+++ golang/fizz_buzz.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func fizzBuzz(n int) []string {
+	r := make([]string, 0)
+
+	for i := 1; i <= n; i++ {
+		var s string
+		if i%3 == 0 {
+			s = "Fizz"
+		}
+		if i%5 == 0 {
+			s += "Buzz"
+		}
+		if len(s) == 0 {
+			s = strconv.Itoa(i)
+		}
+
+		r = append(r, s)
+	}
+
+	return r
+}
+
+func main() {
+	for _, v := range fizzBuzz(15) {
+		fmt.Println(v)
+	}
+}
