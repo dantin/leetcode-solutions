a410cf698b7faa694074f4d588955cdcfe6cd10f
david.ding <chengjie.ding@gmail.com>
2017-06-08 12:02:44 +0800

    add solution of keybord row

---
+++ golang/keyboard_row.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func findWords(words []string) []string {
+	keybord := [][]rune{
+		[]rune{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'},
+		[]rune{'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'},
+		[]rune{'z', 'x', 'c', 'v', 'b', 'n', 'm'},
+	}
+	char_map := make(map[rune]int)
+	for row, chars := range keybord {
+		for _, c := range chars {
+			char_map[c] = row
+			char_map[unicode.ToUpper(c)] = row
+		}
+	}
+
+	res := make([]string, 0)
+	for _, word := range words {
+		cs := []rune(word)
+		row := char_map[cs[0]]
+		f := true
+		for _, c := range cs {
+			if char_map[c] != row {
+				f = false
+				break
+			}
+		}
+		if f {
+			res = append(res, word)
+		}
+	}
+
+	return res
+}
+
+func main() {
+	words := []string{"Hello", "Alaska", "Dad", "Peace"}
+
+	fmt.Println(words)
+	fmt.Println(findWords(words))
+}
